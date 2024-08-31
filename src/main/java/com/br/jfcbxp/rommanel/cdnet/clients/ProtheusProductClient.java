db513e8b53d111bd8c06f2706c32038d9c378aeb
jfcbxp <jfcbxp@gmail.com>
2024-08-31 10:52:55 -0300

    ♻️ refactor: upd ProtheusProductClient

--- src/main/java/com/br/jfcbxp/rommanel/cdnet/clients/ProtheusProductClient.java
+++ src/main/java/com/br/jfcbxp/rommanel/cdnet/clients/ProtheusProductClient.java
@@ -5,6 +5,7 @@ import com.br.jfcbxp.rommanel.cdnet.records.requests.ProtheusRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,7 +20,9 @@ public interface ProtheusProductClient {
     Logger log = LoggerFactory.getLogger(ProtheusProductClient.class);
 
     @PostMapping(
-            value = "/estcdnet"
+            value = "/estcdnet",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     void sendProductInfo(
             @RequestBody()
